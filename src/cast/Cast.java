53861ece6079928d239bba1a18a0c1cdffc7d001
Lenin Chasi <64288777+lchasi@users.noreply.github.com>
2020-05-03 21:20:04 -0500

    primari summary

---
+++ src/cast/Cast.java
@@ -0,0 +1,27 @@
+//Un casting se indica entre paréntesis con el tipo de dato al que queremos pasar, en este caso int.
+//La variable a, contendrá un 2 y no un 2.5. No se puede hacer castings entre todos los tipos, por ejemplo
+//entre un int y un String. El error que aparece es este Cannot cast from (dato1) to (dato2).
+
+package cast;
+
+public class Cast {
+
+   
+    public static void main(String[] args) {
+        
+        double a = 5 ;
+        int i ;
+        i = (int)a;
+      
+        
+       
+        
+        
+     System.out.println(i); {   
+    }
+     
+    }     
+}  
+        
+  
+   
